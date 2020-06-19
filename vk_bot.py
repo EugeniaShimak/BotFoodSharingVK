882c2ec4304cc1ceae858c10cc21718a4ccbc1f0
EugeniaShimak <greenlighty24@gmail.com>
2020-06-19 17:47:54 +0500

    initial commit

---
+++ vk_bot.py
@@ -0,0 +1,43 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message, 'random_id': vk_api.utils.get_random_id()})
+
+
+
+# API-ключ созданный ранее
+token = "efe0a9f25d635fe261993a179d919306953ba7c117a7756a26e22e1f54be8259996586139c128ae652e0b"
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token=token)
+
+# Работа с сообщениями
+longpoll = VkLongPoll(vk)
+
+
+# класс комманд
+class Commands:
+    hi = ["привет"]
+    by = ["пока"]
+
+# Основной цикл
+for event in longpoll.listen():
+
+    # Если пришло новое сообщение
+    if event.type == VkEventType.MESSAGE_NEW:
+
+        # Если оно имеет метку для меня( то есть бота)
+        if event.to_me:
+
+            # Сообщение от пользователя
+            request = event.text
+
+            # Каменная логика ответа
+            if request == "привет":
+                write_msg(event.user_id, "Хай")
+            elif request == "пока":
+                write_msg(event.user_id, "Пока((")
+            else:
+                write_msg(event.user_id, "Не поняла вашего ответа...")
