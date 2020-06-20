604ffe428c712652568f3211e75a5e0262f87d06
EugeniaShimak <greenlighty24@gmail.com>
2020-06-20 20:57:12 +0500

    добавлен набросок клавиатуры

--- vk_bot.py
+++ vk_bot.py
@@ -3,47 +3,128 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 from Auth import vk_api_auth_community, vk_api_auth_service
 from urllib.parse import urlparse
-
+import json
 
 # отправка сообщния
-def write_msg(user_id, message):
+def write_msg(user_id, message, keyboard):
     vk_api_auth_community.method('messages.send',
-    {'user_id': user_id, 'message': message, 'random_id': vk_api.utils.get_random_id()})
+                                 {'user_id': user_id,
+                                  'message': message,
+                                  'random_id': vk_api.utils.get_random_id(),
+                                  'keyboard': json.dumps(keyboard)})
+
+
+# класс комманд
+class Commands:
+    community = "сообщества:"
+
+
+class Answer:
+    success_comm = "Сообщества добавлены успешно!\n"
+    wrong_comm = "Не могу разобрать адреса сообществ, попробуй еще раз!\n"
+    choose_category_location = "Выбери категории, которые ты хочешь отслеживать и локацию! Как закончишь добвлять категории, нажми на кнопку 'Закончить'"
+    standard = "Приветствую! Это ФудБот, который поможет тебе получать уведомления об объявлениях в фудшеринговых сообществах! \n Для начала отправь список сообществ через запятую, из которых хочешь получать объявления со словом 'сообщества:' и двоеточием в начале сообщения. \n Пример: сообщества: https://vk.com/murmewmur, https://vk.com/public196448452 \n"
+
+
+# список сообществ юзера
+listComm = []
+
+
+# парсинг сообщения от пользователя с сообществами
+def get_communities_from_request(str):
+    if str.split(Commands.community)[1]:
+        list_comm = str.split(Commands.community)[1].split(",")
+        if len(list_comm) > 0:
+            return list_comm
+        else:
+            return []
+    else:
+        return []
+
+
+# Добавление сообществ
+def add_communities_user(request):
+    list_communities = []
+    if len(get_communities_from_request(request)) > 0:
+        for comm in get_communities_from_request(request):
+            list_communities.append(comm)
+            # тут надо схранять в бд
+            print(list_communities)
+        write_msg(event.user_id, Answer.success_comm + Answer.choose_category_location,
+                  {"buttons": [[
+                      {
+                          "action": {
+                              "type": "location",
+                              "payload": "{\"button\": \"1\"}"
+                          }
+                      }
+                  ],
+                      [
+                          {
+                              "action": {
+                                  "type": "text",
+                                  "payload": "{\"button\": \"0\"}",
+                                  "label": "ЗАКОНЧИТЬ"
+                              },
+                              "color": "positive"
+                          }],
+                [
+                    {
+                        "action": {
+                            "type": "text",
+                            "payload": "{\"button\": \"2\"}",
+                            "label": "Фрукты"
+                        },
+                        "color": "primary"
+                    }],
+                    [{
+                        "action": {
+                            "type": "text",
+                            "payload": "{\"button\": \"3\"}",
+                            "label": "Овощи"
+                        },
+                        "color": "primary"
+                    }],
+                    [{
+                        "action": {
+                            "type": "text",
+                            "payload": "{\"button\": \"4\"}",
+                            "label": "Крупы"
+                        },
+                        "color": "primary"
+                    }
+                ]], "one_time": False})
+    else:
+        write_msg(event.user_id, Answer.wrong_comm, {})
+        write_msg(event.user_id, Answer.standard, {})
+    return list_communities
 
 
 # Работа с сообщениями
 longpoll = VkLongPoll(vk_api_auth_community)
 
-
-# класс комманд
-class Commands:
-    hi = ["привет"]
-    by = ["пока"]
-
 # Основной цикл
-    for event in longpoll.listen():
+for event in longpoll.listen():
+    # Если пришло новое сообщение
+    if event.type == VkEventType.MESSAGE_NEW:
 
-        # Если пришло новое сообщение
-        if event.type == VkEventType.MESSAGE_NEW:
+        # Если оно имеет метку для меня( то есть бота)
+        if event.to_me:
 
-            # Если оно имеет метку для меня( то есть бота)
-            if event.to_me:
+            # Сообщение от пользователя
+            request = event.text.lower()
 
-                # Сообщение от пользователя
-                request = event.text
-
-                # Каменная логика ответа
-                if request == "привет":
-                    write_msg(event.user_id, "Хай")
-                elif request == "пока":
-                    write_msg(event.user_id, "Пока((")
-                else:
-                    write_msg(event.user_id, "Не поняла вашего ответа...")
+            # Каменная логика ответа
+            if request.startswith(Commands.community):
+                add_communities_user(request)
+            else:
+                write_msg(event.user_id, Answer.standard, {})
 
 
 # функция обрезания url группы, чтобы остался только идентификатор или короткое имя
 def format_group_url(group_url):
-    short_name = urlparse(group_url).path.split('/')[1]  # обрезаем все что до слэша справа нпрмр https://vk.com/murmewmur' станет murmewmur
+    # обрезаем все что до слэша справа нпрмр https://vk.com/murmewmur' станет murmewmur
+    short_name = urlparse(group_url).path.split('/')[1]
     print(short_name)
     return short_name
 
@@ -65,11 +146,9 @@ def get_post(group_name):
         print("post" + str(post))
         return post
 
-
-# тестируемая группа
-url = "https://vk.com/murmewmur"
-# короткое имя
-name_group = format_group_url(url)
-# получаем пост по короткому имени сообщества
-get_post(name_group)
-
+# # тестируемая группа
+# url = "https://vk.com/murmewmur"
+# # короткое имя
+# name_group = format_group_url(url)
+# # получаем пост по короткому имени сообщества
+# get_post(name_group)
